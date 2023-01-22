73a6a5f0869b70332b9b437c471c14f668590176
kassiber10 <almailreciever@gmail.com>
2023-01-22 23:30:31 +0100

    Changed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue-inn-booking/",
   plugins: [vue()],
   server: {
     watch: { usePolling: true },
